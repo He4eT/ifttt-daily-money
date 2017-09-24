b21313dd5134a8b7be70b9e625d061c4587f1889
He4eT <He4eTHb1u@gmail.com>
2017-09-24 22:48:27 +0500

    Cleanup

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express')
+var bodyParser = require('body-parser')
 
-var app = express();
-app.set('port', (process.env.PORT || 5000));
+var app = express()
+app.set('port', (process.env.PORT || 5000))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.param('day', addParam('day'))
+app.param('key', addParam('key'))
+app.param('money', addParam('money'))
 
 app.get('/', function (req, res) {
-  res.redirect('http://example.com');
+  res.redirect('http://example.com')
 })
 
-app.param('day', function(req, res, next, day) {
-  req.day = day
-  next() 
-})
-
-app.param('key', function(req, res, next, key) {
-  req.key = key
-  next() 
-})
-
-app.get('/result/:key/:day', function (req, res) {
+app.get('/result/:key/:day/:money', function (req, res) {
+  var money = parseInt(req.money)
   var nextPayDay = parseInt(req.day)
+  var iftttKey = req.key
 
-  res.send({
-    n: getDifference(nextPayDay),
-    k: req.key
-  })
+  var params = {
+    days: getDifference(nextPayDay),
+    money: money, 
+    key: iftttKey
+  }
+
+  res.send(params)
+  checkMoney(params)
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
+
+function addParam (paramName) {
+  return function (req, res, next, param) {
+    req[paramName] = param
+    next()
+  }
+}
 
 function getDifference (paydayNumber) {
   var now = new Date()
@@ -44,7 +51,7 @@ function getDifference (paydayNumber) {
     new Date(year, month, day)
 
   var payday =
-      new Date(year, month, paydayNumber)
+    new Date(year, month, paydayNumber)
 
   var inThisMonth = today <= payday
 
@@ -55,6 +62,10 @@ function getDifference (paydayNumber) {
   return numberOfDays
 }
 
-function dayInMonth(month, year) {
+function dayInMonth (month, year) {
   return 32 - new Date(year, month, 32).getDate()
+}
+
+function checkMoney (args) {
+  console.log({args})
 }
